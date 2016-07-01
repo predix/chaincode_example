aca39b249adece07290e3ab78c9188d9026a3987
Atul Kshirsagar <atul.kshirsagar@gmail.com>
2016-06-30 17:04:01 -0700

    Add another chaincode and restructure
    Signed-off-by: Leo Chu <leocchu@gmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+#Chaincode
+This repo has sample chain code that can be deployed on hyperledger fabric.
---
+++ infinite_chaincode/infinite_chaincode.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// this chaincode's job is to run an infinite loop of putting a state.
+// this is to test out how fabric behaves in case of a buggy/malicious chaincode
+type InfiniteChaincode struct {
+}
+
+func (t *InfiniteChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	var A string // Entities
+	var Aval int // Asset holdings
+	var err error
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+
+	// Initialize the chaincode
+	A = args[0]
+	Aval, err = strconv.Atoi(args[1])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding")
+	}
+	fmt.Printf("Aval = %d\n", Aval)
+
+	// Write the state to the ledger
+	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// Transaction makes payment of X units from A to B
+func (t *InfiniteChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+
+	var X int // Transaction value
+	var err error
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+
+	var A string
+	var Aval int // Asset holdings
+	A = args[0]
+	X, err = strconv.Atoi(args[1])
+	if err != nil {
+		return nil, errors.New("Invalid amount to increment")
+	}
+
+	// Get the state from the ledger
+	// TODO: will be nice to have a GetAllState call to ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	if Avalbytes == nil {
+		return nil, errors.New("Entity not found")
+	}
+	Aval, _ = strconv.Atoi(string(Avalbytes))
+
+	// Perform the execution
+	for {
+		Aval = Aval + X
+		fmt.Printf("Aval = %d\n", Aval)
+
+		// Write the state back to the ledger
+		stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	}
+
+	return nil, nil
+}
+
+// Query callback representing the query of a chaincode
+func (t *InfiniteChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	if function != "query" {
+		return nil, errors.New("Invalid query function name. Expecting \"query\"")
+	}
+	var A string // Entities
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
+	}
+
+	A = args[0]
+
+	// Get the state from the ledger
+	Avalbytes, err := stub.GetState(A)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	if Avalbytes == nil {
+		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return Avalbytes, nil
+}
+
+func main() {
+	err := shim.Start(new(InfiniteChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
