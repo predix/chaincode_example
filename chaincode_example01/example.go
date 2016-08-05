9b39655c1007a6709c095e928f3319b2a1f89fc9
Atul Kshirsagar <atul.kshirsagar@gmail.com>
2016-08-04 17:38:32 -0700

    Remove unnecessary comments

--- chaincode_example01/example.go
+++ chaincode_example01/example.go
@@ -1,11 +1,5 @@
 package main
 
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
-
 import (
 	"errors"
 	"fmt"
