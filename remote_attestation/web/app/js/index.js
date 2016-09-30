5c8f7908cda648877f7fc8ebfad65de2beb3b79f
Jonnie Spratley <jonnie.spratley@ge.com>
2016-09-30 15:20:30 -0700

    added basic ui and socket for refreshing

--- remote_attestation/web/app/js/index.js
+++ remote_attestation/web/app/js/index.js
@@ -1,45 +1,8 @@
 document.addEventListener('WebComponentsReady', function() {
-
-	Polymer({
-		is: 'x-app',
-		properties: {
-			listData: {
-				type: Array
-			}
-		},
-		ready: function() {
-			console.warn(this.tagName, ' created!');
-		},
-		attached: function() {
-			console.warn(this.tagName, ' attached!');
-		},
-		detached: function() {
-			console.warn(this.tagName, ' detached!');
-		},
-		formatDate: function(d){
-			var date = moment(d / 1000000).format('llll');
-			console.log('formatDate', d, date);
-			return date;
-		}
-	});
-
-/**
-html2canvas(document.body, {
-  onrendered: function(canvas) {
-    document.body.appendChild(canvas);
-  }
-});*/
 	var app = document.querySelector('#app');
-var data = {
-    "jsonrpc": "2.0",
-    "result": {
-        "status": "OK",
-        "message": "[{\"device_id\":\"client-1\",\"attestation_serve_id\":\"server-1\",\"status\":2,\"validation_hash\":\"abc13f5ed82b868699041677f80a5e879af2fc78\",\"time\":1475083802894940886},{\"device_id\":\"device1\",\"attestation_serve_id\":\"server1\",\"status\":1,\"validation_hash\":\"0osdfgs9uodmvnp4wcw7\",\"time\":1475012947665151410},{\"device_id\":\"device2\",\"attestation_serve_id\":\"server2\",\"status\":2,\"validation_hash\":\"0osdfgsrtty678mvnp4wcw7\",\"time\":1475084648581502932}]"
-    },
-    "id": 0
-};
-
-var devices = JSON.parse(data.result.message);
-	app.data = devices;
-	console.log(devices);
+	var socket = io.connect(window.location.href);
+	socket.on('devices', function(data) {
+		console.log('Updating data...', data);
+		app.set('devices', data);
+	});
 });
--- remote_attestation/web/server.js
+++ remote_attestation/web/server.js
@@ -9,7 +9,11 @@ const log = require('npmlog');
 const pkg = require(path.resolve(__dirname, './package.json'));
 const config = pkg.config;
 const PORT = process.env.PORT || process.env.npm_package_config_port;
+
 const app = express();
+
+const BLOCKCHAIN_REFRESH_INTERVAL = process.env.BLOCKCHAIN_REFRESH_INTERVAL || 5000;
+
 
 
 var program = {
@@ -34,6 +38,47 @@ program.config.get('routes').forEach(function(route) {
 	require(path.resolve(__dirname, route))(program, app);
 });
 
-app.listen(PORT, function() {
-	program.log.info('Open your browser to http://localhost:' + PORT);
+
+
+app.use(require('morgan')('dev'));
+app.set('port', process.env.PORT || process.env.npm_package_config_port || 9003);
+
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+/*
+app.listen(app.get('port'), function() {
+	program.log.info('Open your browser to http://localhost:' + app.get('port'));
+});*/
+
+
+
+const remoteAttestation = require('./remote-attestation');
+
+const pollDevices = (socket) =>{
+	let refreshDevices = setInterval(() =>{
+		log.info('refreshDevices');
+		remoteAttestation.getDevices().then((resp) =>{
+			socket.emit('devices', resp);
+		});
+	}, BLOCKCHAIN_REFRESH_INTERVAL);
+
+	socket.on('disconnect', () =>{
+		log.info('disconnect', 'socket disconnected clearing interval');
+		clearInterval(refreshDevices);
+	});
+};
+
+
+io.on('connection', (socket) => {
+	pollDevices(socket);
+	remoteAttestation.getDevices().then((resp) =>{
+		socket.emit('devices', resp);
+	});
+
 });
